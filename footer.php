d16eedcbd277841ea86e65f4c57741e119a0f3f2
Ian Stevens <istevens@ian-stevens-powerbook-g4-12.local>
2009-01-08 15:17:58 -0500

    Cleaned up 3-panel footer code

--- footer.php
+++ footer.php
@@ -5,40 +5,19 @@
  */
 ?>
 
+<?php if ( function_exists('dynamic_sidebar') ) {?>
 <div class="yui-gb">
     <div class="yui-u first">
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('left') ) : ?>
-            <h2>About</h2>
-            <p>Ian Stevens is a software developer living in Toronto, Canada.</p>
-        <?php endif; ?>
+        <?php dynamic_sidebar('left') ?>
     </div>
     <div class="yui-u">
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('middle') ) : ?>
-            <h2>Elsewhere</h2>
-            <p>Visit me on the following networks:</p>
-            <ul>
-                <li>Facebook</li>
-                <li>Twitter</li>
-                <li>Delicious</li>
-                <li>LinkedIn</li>
-                <li>ClaimID</li>
-            </ul>
-            <p>And using the following IM services:</p>
-            <ul>
-                <li>MSN</li>
-                <li>GMail</li>
-                <li>Yahoo</li>
-                <li>AIM</li>
-                <li>ICQ<//li>
-            </ul>
-        <?php endif; ?>
+        <?php dynamic_sidebar('middle') ?>
     </div>
     <div class="yui-u">
-        <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('right') ) : ?>
-            <h2>Recent Comments</h2>
-        <?php endif; ?>
+        <?php dynamic_sidebar('right') ?>
     </div>
 </div>
+<?php } ?>
 
 <div id="ft">
 </div>
