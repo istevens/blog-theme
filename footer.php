fc9a6677520dc64bc1b0118456b3479acd325e5f
Ian Stevens <istevens@ian-stevens-powerbook-g4-12.local>
2009-01-08 16:23:29 -0500

    Moved sitemeta into footer. Added styling.

--- footer.php
+++ footer.php
@@ -5,24 +5,24 @@
  */
 ?>
 
-<?php if ( function_exists('dynamic_sidebar') ) {?>
-<div class="yui-gb">
-    <div class="yui-u first">
-        <?php dynamic_sidebar('left') ?>
-    </div>
-    <div class="yui-u">
-        <?php dynamic_sidebar('middle') ?>
-    </div>
-    <div class="yui-u">
-        <?php dynamic_sidebar('right') ?>
-    </div>
 </div>
-<?php } ?>
 
 <div id="ft">
+    <?php if ( function_exists('dynamic_sidebar') ) {?>
+    <div id="sitemeta" class="yui-gb">
+        <div class="yui-u first">
+            <?php dynamic_sidebar('left') ?>
+        </div>
+        <div class="yui-u">
+            <?php dynamic_sidebar('middle') ?>
+        </div>
+        <div class="yui-u">
+            <?php dynamic_sidebar('right') ?>
+        </div>
+    </div>
+    <?php } ?>
 </div>
 
-</div>
 </div>
 		<?php wp_footer(); ?>
 </div>
