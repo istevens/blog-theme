cb642cde52de2cf1bb943881d3c5ddc217727acf
Ian Stevens <istevens@ian-stevens-powerbook-g4-12.local>
2010-01-19 23:31:28 -0500

    Added links to sidebar, with optional content. Link post styled similar to tweets with different wording.

--- index.php
+++ index.php
@@ -9,14 +9,32 @@ get_header(); $page = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
 <div class="yui-gd">
 
 <?php if(is_home() and !get_query_var('paged')) { ?>
-<div class="side yui-u first"><div id="tweets">
+<div class="side yui-u first"><div id="online">
     <h2>Latest Online Activity</h2>
-    <?php $first = $page == 1 ? 'first-post': ''; query_posts('category_name=tweets&showposts=10'); ?>
+    <?php $first = $page == 1 ? 'first-post': ''; query_posts('cat=32,33&showposts=10'); ?>
     <?php while (have_posts()) : the_post(); ?>
         <div <?php post_class($first . " entry"); $first = ''; ?> >
-            <h3><?php echo get_the_content() ?></h3>
+            <?php if (in_category('32')) {
+                $title = get_the_content();
+                $content = "";
+                $link = "http://twitter.com/istevens/status/".get_post_meta($post->ID, 'aktt_twitter_id', 'true');
+                $link_verb = "See";
+                $link_text = " on Twitter";
+                $verb = "Tweeted";
+            } else if (in_category('33')) {
+                $link = get_post_meta($post->ID, 'link', 'true');
+                $title = "<a href=".$link.">".$post->post_title."</a>";
+                $content = get_the_content();
+                $link_verb = "Go to";
+                $link_text = "";
+                $verb = "Linked";
+            } ?>
+            <h3><?php echo $title; ?></h3>
+            <?php if ($content != "") { 
+                echo "<p>".$content."</p>";
+            } ?>
             <p class="postmeta">
-                <a href="http://twitter.com/istevens/status/<?php echo get_post_meta($post->ID, 'aktt_twitter_id', 'true') ?>" title="See '<?php the_title_attribute(); ?>' on Twitter">On Twitter on <?php the_time('Y-m-d \a\t G:i') ?></a>
+                <a href="<?php echo $link ?>" title="<?php echo $link_verb; ?> '<?php the_title_attribute(); echo "'".$link_text."\">".$verb ?> on <?php the_time('Y-m-d \a\t G:i') ?></a>
                 <?php comments_popup_link('Comment &#187;', '1 Comment &#187;', '% Comments &#187;', 'comments'); ?>
             </p>
         </div>
@@ -26,7 +44,7 @@ get_header(); $page = (get_query_var('paged')) ? get_query_var('paged') : 1; ?>
 
 <div id="blog" class="yui-u">
     <?php $first = $page == 1 ? 'first-post': ''; ?>
-    <?php query_posts('cat=-32&paged=' . $page); ?>
+    <?php query_posts('cat=-32,-33&paged=' . $page); ?>
     <?php while (have_posts()) : the_post(); ?>
 
         <div <?php post_class($first) ?> id="post-<?php the_ID(); ?>">
