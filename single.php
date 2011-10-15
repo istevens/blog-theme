628d690d7461f56475ee5c6adb1dd5d93e487608
Ian Stevens <istevens@ian-stevens-powerbook-g4-12.local>
2011-10-15 17:07:14 -0400

    Using proper date element for single post.

--- single.php
+++ single.php
@@ -31,7 +31,7 @@ get_header();
                 You'll need to download this plugin, and follow the instructions:
                 http://binarybonsai.com/archives/2004/08/17/time-since-plugin/ */
                 /* $entry_datetime = abs(strtotime($post->post_date) - (60*120)); echo time_since($entry_datetime); echo ' ago'; */ ?>
-            on <?php the_time('l, F jS, Y') ?> at <?php the_time() ?>
+            on <date pubdate datetime="<?php the_time('Y-m-d\TH:i:s\Z'); ?>"><?php the_time('l, F jS, Y') ?> at <?php the_time() ?></date>
             and is filed under <?php the_category(', ') ?>.
             You can follow any responses to this entry through the <?php post_comments_feed_link('RSS 2.0'); ?> feed.
 
