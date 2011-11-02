440da3a776bc64ff78ec084d61b6d25513ea7f82
Ian Stevens <istevens@powerbook.crazed>
2011-11-01 20:54:16 -0400

    Inter-article spacing wasn't Fibonacci due to para padding. Adjusting article bottom padding so that it plus para padding is Fib.

--- index_post.php
+++ index_post.php
@@ -24,7 +24,7 @@
             echo $content;
         } ?>
         <footer>
-            <a class="external" href="<?php echo $link ?>" title="<?php echo $link_verb; ?> '<?php the_title_attribute(); echo "'".$link_text."\"><span class=\"verb\">".$verb ?> on</span> <time pubdate datetime="<?php the_time(XML_DATE); ?>"><?php the_time('M j \a\t G:i') ?></time></a>
+            <p><a class="external" href="<?php echo $link ?>" title="<?php echo $link_verb; ?> '<?php the_title_attribute(); echo "'".$link_text."\"><span class=\"verb\">".$verb ?> on</span> <time pubdate datetime="<?php the_time(XML_DATE); ?>"><?php the_time('M j \a\t G:i') ?></time></a></p>
         </footer>
 
     <?php } else { ?>
