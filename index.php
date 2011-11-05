fabf0e8af484f7739e40bfaebea13544926a74d1
Ian Stevens <istevens@powerbook.crazed>
2011-11-05 03:01:36 -0400

    Posts I've taken the time to categorise likely deserve to be featured as blog entries rather than links. Will have to profile these queries.
    We now have link-styled entries mixing in with blog-styled entries. The dates
    are in different spots, and there's a discrepancy between what the title links
    to. Need to be consisten.

--- index.php
+++ index.php
@@ -8,6 +8,23 @@ get_header();
 $page = (get_query_var('paged')) ? get_query_var('paged') : 1;
 $paged = $page;
 $home = is_home() and !get_query_var('paged');
+$cats = get_all_category_ids();
+
+/**
+ * This is why I need to built my own custom weblog, so I can have better 
+ * control over the customization without a CPU cost.
+ *
+ * If I take the time to category a post, it probably should be on the front 
+ * page, not twice.
+ */
+$tweets = 32;
+$links = 33;
+$feature_posts = $cats;
+foreach($feature_posts as $i => $c) {
+    if($c == $tweets || $c == $links) {
+        unset($feature_posts[$i]);
+    }
+}
 ?>
 
 <section id="content">
@@ -15,7 +32,7 @@ $home = is_home() and !get_query_var('paged');
 <section id="blog">
     <?php
         $first = $page == 1 ? 'first-post': '';
-        query_posts('cat=-32,-33&showposts=5&paged=' . $paged);
+        query_posts(array('category__in' => $feature_posts, 'showposts' => 5, 'paged' => $paged));
         while (have_posts()) : the_post();
             include('index_post.php');
             $first = '';
@@ -28,12 +45,14 @@ $home = is_home() and !get_query_var('paged');
     <?php posts_nav_link(' ', 'Newer', 'Older') ?>
 </nav>
 
-<?php if($home) { ?>
+<?php
+    $online_query = array('showposts' => 3, 'paged' => $paged);
+    if($home) { ?>
 <section id="online">
     <section id="mentioned">
         <h1 class="fancy">On My Mind</h1>
         <?php
-            query_posts('cat=32&showposts=3&paged=' . $paged);
+            query_posts(array('cat' => 32) + $online_query);
             while (have_posts()) : the_post(); 
                 include('index_post.php');
                 $first = '';
@@ -44,7 +63,7 @@ $home = is_home() and !get_query_var('paged');
     <section id="read">
         <h1 class="fancy">Recently Read</h1>
         <?php
-            query_posts('cat=33&showposts=3&paged='. $paged);
+            query_posts(array('cat' => 33, 'category__not_in' => $feature_posts) + $online_query);
             while (have_posts()) : the_post(); 
                 include('index_post.php');
                 $first = '';
